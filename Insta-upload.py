6d63bd333fe5604b200930c5a28171f445fbad67
Maheshishere <108296592+MaheshTheDeveloper@users.noreply.github.com>
2022-12-24 22:32:32 +0530

    v2.1
    It can now upload multiple post

--- Insta-upload.py
+++ Insta-upload.py
@@ -1,6 +1,9 @@
 from myigbot import MyIGBot
 
-bot = MyIGBot('xxxxxxxxx', 'xxxxxxxxx')
+bot = MyIGBot('daily_.quoted', 'maheshishere9689')
 
-upload = bot.upload_post('1.jpg', caption='Drop a like for daily quote. #quote')
-print(upload)  # if the response code is 200 that means ok
+image_filenames = ['2.jpg', '3.jpg', '4.jpg']
+
+for image_filename in image_filenames:
+    upload = bot.upload_post(image_filename, caption='Drop a like for daily quote. #quote')
+    print(upload)  # if the response code is 200 that means ok
