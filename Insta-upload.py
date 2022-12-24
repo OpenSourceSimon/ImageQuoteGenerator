318c6bd6e68b653d9513a8c6835325919b2b96d1
Maheshishere <108296592+MaheshTheDeveloper@users.noreply.github.com>
2022-12-24 22:33:28 +0530

    deleting

--- Insta-upload.py
+++
@@ -1,9 +0,0 @@
-from myigbot import MyIGBot
-
-bot = MyIGBot('daily_.quoted', 'maheshishere9689')
-
-image_filenames = ['2.jpg', '3.jpg', '4.jpg']
-
-for image_filename in image_filenames:
-    upload = bot.upload_post(image_filename, caption='Drop a like for daily quote. #quote')
-    print(upload)  # if the response code is 200 that means ok
